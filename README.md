fecfa2fbea83a5a9fd72e9a41f8e77f29849aea4
Steve Martinelli <s.martinelli@gmail.com>
2017-11-24 14:15:25 -0500

    init repo

---
+++ README.md
@@ -0,0 +1,13 @@
+# Python Flask Hello World Sample
+
+This application demonstrates a simple, reusable Python web application based on the [Flask microframework](http://flask.pocoo.org/).
+
+## Run the app locally
+
+1. [Install Python][]
+1. cd into this project's root directory
+1. Run `pip install -r requirements.txt` to install the app's dependencies
+1. Run `python welcome.py`
+1. Access the running app in a browser at <http://localhost:5000>
+
+[Install Python]: https://www.python.org/downloads/
---
+++ welcome.py
@@ -0,0 +1,45 @@
+# Copyright 2015 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def Welcome():
+    return app.send_static_file('index.html')
+
+@app.route('/myapp')
+def WelcomeToMyapp():
+    return 'Welcome again to my app running on Bluemix!'
+
+@app.route('/api/people')
+def GetPeople():
+    list = [
+        {'name': 'John', 'age': 28},
+        {'name': 'Bill', 'val': 26}
+    ]
+    return jsonify(results=list)
+
+@app.route('/api/people/<name>')
+def SayHello(name):
+    message = {
+        'message': 'Hello ' + name
+    }
+    return jsonify(results=message)
+
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=int(port))
